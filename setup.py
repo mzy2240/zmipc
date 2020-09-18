1e77bbb14439dd8475f8a201701b83ccc2e6d010
mzy2240 <mao.mzy@gmail.com>
2020-09-18 00:31:14 -0500

    1. Clean up.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='zmipc',
-      version='0.2',
+      version='0.5',
       description='A zero-copy memory-sharing IPC on the top of mmap',
       long_description=long_description,
       long_description_content_type="text/markdown",
